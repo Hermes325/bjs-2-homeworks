4949337a9bc5c04c9e93edaf987e4ae547aecbd2
Синохчян Георгий <georg3georg3georg@gmail.com>
2023-08-01 00:40:13 +0300

    исправления

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,24 +4,23 @@ function solveEquation(a, b, c) {
   let d = b ** 2 - 4 * a * c;
   let x1;
   let x2;
-  if (d < 0) {
-    arr = [];
-  } else if (d == 0) {
-    x1 = -b / (2 * a);
-    arr = [x1];
-  } else {
+  if (d > 0) {
     x1 = (-b + Math.sqrt(d)) / (2 * a);
     x2 = (-b - Math.sqrt(d)) / (2 * a);
     arr = [x1, x2];
+  } else if (d == 0) {
+    x1 = -b / (2 * a);
+    arr = [x1];
   }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  percent /= 1200;
+  monthPercent = percent / 1200;
   let loans = amount - contribution;
   let payment =
-    loans * (percent + percent / ((1 + percent) ** countMonths - 1));
+    loans *
+    (monthPercent + monthPercent / ((1 + monthPercent) ** countMonths - 1));
   let allAmount = payment * countMonths;
-  return allAmount.toFixed(2);
+  return Number(allAmount.toFixed(2));
 }
