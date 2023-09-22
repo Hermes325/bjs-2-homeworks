ae1e963c603284ba5f71af49e4c109391197b1a3
Синохчян Георгий <georg3georg3georg@gmail.com>
2023-09-22 11:34:05 +0300

    не понимаю

---
+++ 7.async/task.js
@@ -0,0 +1,35 @@
+class AlarmClock {
+  constructor() {
+    this.alarmCollection = [];
+    this.intervalId;
+  }
+  addClock(time, callback) {
+    if (this.alarmCollection.includes(time) === true) {
+      console.warn("Уже присутствует звонок на это же время");
+    }
+    this.alarmCollection.push([time, callback]);
+  }
+  getCurrentFormattedTime() {
+    let newDate = new Date();
+    let hours = newDate.getHours();
+    let minutes = newDate.getHours();
+    return `Текущее время: ${hours}:${minutes}`;
+  }
+
+  removeClock(time) {
+    // this.alarmCollection.filter((clock) => clock !== time);
+    let index = this.alarmCollection.indexOf(time);
+    if (index !== -1) {
+      this.alarmCollection.splice(index, 1);
+    }
+    return this;
+  }
+}
+
+// addClock(time) {
+//     // if (hours > 23 || minutes > 59 || ((minutes || hours) < 0)) {
+//     //   console.log("не то");
+//     // } else {
+//       this.alarmCollection.push(time);
+//     // }
+//   }
